9e093ae45b8602052bb9a9c43d7405b471b34f72
Cédric Bohnert <cbohnert.datascience@gmail.com>
2022-03-22 11:49:01 +0100

    more added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,36 @@
-var turn = "joueur1";
+var turn = true;
 var grid = [[0,0,0], [0,0,0], [0,0,0]];
-var cases = 9;
+var left = 9;
+var circle = `<svg width="103" height="103"><circle cx="55" cy="49" r="44" fill="none" stroke="blue" stroke-width="3" /></svg>`;
+var cross = `<svg width="103" height="103"><path d="M 10 10 L 90 90" stroke="blue" stroke-width="3"/><path d="M 90 10 L 10 90" stroke="blue" stroke-width="3"/></svg>`;
+
+
 
 initListeners();
 
 
-
+// Add event listeners to the game cells
 function initListeners() {
-    for (let row = 0;  row < 3; row++) {
-        for (let col = 0; col < 3; col++) {
-            document.getElementById(""+(row+1)+""+(col+1)).addEventListener('click', showMark);
+    let rows = document.getElementById("grid").children;
+    for (let row of rows) {
+        for (let cell of row.cells) {
+            document.getElementById(cell.id).addEventListener('click', playTurn);
         }
     }
-
 }
+
+// Remove event listeners to the game cells
 function remListeners() {
-    for (let row = 0;  row < 3; row++) {
-        for (let col = 0; col < 3; col++) {
-            document.getElementById(""+(row+1)+""+(col+1)).removeEventListener('click', showMark);
+    let rows = document.getElementById("grid").children;
+    for (let row of rows) {
+        for (let cell of row.cells) {
+            document.getElementById(cell.id).removeEventListener('click', playTurn);
         }
     }
-
+    
 }
 
-
+// check whether game is won or draw
 function isWon() {
     let sums = [];
     for (let i = 0; i < grid.length; i++) {
@@ -44,73 +51,64 @@ function isWon() {
     
     let max = Math.max.apply(null, sums);
     let min = Math.min.apply(null, sums);
-    console.log(sums);
-    if (max === 3 && cases != 9) {
-        console.log("joueur1 !");
+    //console.log(sums);
+    if (max === 3 && left != 9) {
+        displayWinner("Joueur 1");
         return true;
     }
-    if (min === -3 && cases !=9) {
-        console.log("joueur2 !");
-        
+    if (min === -3 && left !=9) {
+        displayWinner("Joueur 2");
         return true;
     }
 
     return false;
-    //     let finish = true;
-    //     let zeroCounter = 9;
-    //     for (let i = 0; i < grid.length; i++) {
-    //         for (let j = 0; j < grid.length; j++) {
-    //             if (grid[i][j] === 0) {
-    //                 zeroCounter--;
-    //                 finish=false;
-    //             }
-    //         }
-    //     }
-    //     if (zeroCounter === 0) {
-    //         console.log("Egalité !")
-        // }
-        // return finish;
-    }
+}
 
 
 
 
-
-function showMark(elt) {
-    cases--;
-    if (turn === "joueur1") {
-        elt.target.innerHTML = `<svg width="103" height="103"><circle cx="55" cy="49" r="44" fill="none" stroke="blue" stroke-width="3" /></svg>`;
-        let row = parseInt(elt.target.id[0]);
-        let col = parseInt(elt.target.id[1]);
-        grid[row-1][col-1]=1;
-        turn = "joueur2";
-        if (isWon()) {
-            remListeners();
-            return;
-        }
-        if (!isWon() && cases === 0) {
-            remListeners();
-            console.log('Egalité !');
-            return;
-        }
+// player 1 (circle) or player 2 (cross) plays a turn
+function playTurn(elt) {
+    left--;
+    if (turn === true) {
+        turnAction(elt, circle, 1);
         return;
     } 
-     if (turn === "joueur2") {
-        elt.target.innerHTML = `<svg width="103" height="103"><path d="M 10 10 L 90 90" stroke="blue" stroke-width="3"/><path d="M 90 10 L 10 90" stroke="blue" stroke-width="3"/></svg>`;
-        let row = parseInt(elt.target.id[0]);
-        let col = parseInt(elt.target.id[1]);
-        grid[row-1][col-1]=-1;
-        turn = "joueur1";
-        if (isWon()) {
-            remListeners();
-            return;
-        }
-        if (!isWon() && cases === 0) {
-            remListeners();
-            console.log('Egalité !');
-            return;
-        }
+     if (turn === false) {
+        turnAction(elt, cross, -1);
         return;
     }
 }
 
+function displayWinner(player) {
+   
+    document.getElementsByTagName('p')[0].innerText = `${player} a gagné !`;
+    //document.getElementsByTagName('p')[0].style.display = 'none';
+    //document.getElementById('result').innerText = `${player} a gagné !`;
+    
+}
+
+function displayDraw() {
+    document.getElementsByTagName('p')[0].innerText = "Egalité !";
+    // document.getElementsByTagName('p')[0].style.display = 'none';
+    // document.getElementById('result').innerText = "Egalité !";
+}
+
+function turnAction(elt, svg, player) {
+        elt.target.innerHTML = svg
+        elt.target.removeEventListener('click', playTurn);
+        let row = parseInt(elt.target.id[0]);
+        let col = parseInt(elt.target.id[1]);
+        grid[row-1][col-1]=player;
+        turn = !turn;
+        if (isWon()) {
+            remListeners();
+            return;
+        }
+        if (!isWon() && left === 0) {
+            remListeners();
+            displayDraw();
+            return;
+        }
+        return;
+}
